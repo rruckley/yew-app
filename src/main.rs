128053a6c091de406ebbf068c9be5c3544b7c5a5
Ryan <rruckley@gmail.com>
2022-10-03 22:18:20 +1100

    Now using function_component for video list.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
+
 use yew::prelude::*;
 
+#[derive(Clone,PartialEq)]
 struct Video {
     id: usize,
     title: String,
     speaker: String,
     url: String,
+}
+
+#[derive(Properties, PartialEq)]
+struct VideosListProps {
+    videos: Vec<Video>,
+}
+
+#[function_component(VideosList)]
+fn videos_list(VideosListProps { videos}: &VideosListProps) -> Html {
+    videos.iter().map(|video| html! {
+        <p>{format!("{}: {}", video.speaker, video.title)}</p>
+    }).collect()
 }
 
 #[function_component(App)]
@@ -36,15 +50,12 @@ fn app() -> Html {
             url: "https://youtu.be/PsaFVLr8t4E".to_string(),
         },
     ];
-    let videos = videos.iter().map(|video| html! {
-        <p>{format!("{}: {}", video.speaker, video.title)}</p>
-    }).collect::<Html>();
     html! {
         <>
             <h1>{ "RustConf Explorer" }</h1>
             <div>
                 <h3>{"Videos to watch"}</h3>
-                { videos }
+                <VideosList videos={videos} />
             </div>
             <div>
                 <h3>{ "John Doe: Building and breaking things" }</h3>
